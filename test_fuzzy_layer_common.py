a1b1fa8ea3399d21baa42acfe45bd885b9c22553
gurov <bg@!asdf.r>
2022-11-18 12:37:24 +0300

    add tests

---
+++ test_fuzzy_layer_common.py
@@ -0,0 +1,83 @@
+from fuzzy_layer import FuzzyLayer
+import torch
+
+def test_fromcenters_correct_initialization():
+    model = FuzzyLayer.fromcenters([[1,2,3]])
+    assert model.A.shape == (1,3,4)
+
+def test_fromdimentions_correct_initialization():
+    model = FuzzyLayer.fromdimentions(4,7)
+    assert model.A.shape == (7, 4, 5)
+    
+def test_1d_input_1d_output():
+    size_in = 1
+    size_out = 1
+    x = torch.randn((size_in))
+    initial_scales = torch.randn((size_out, size_in))
+    initial_centers = torch.randn((size_out, size_in))
+
+    model = FuzzyLayer(initial_centers, initial_scales)
+    y_pred = model(x)
+    
+    assert len(y_pred) == size_out
+
+def test_1d_input_2d_output():
+    size_in = 1
+    size_out = 2
+    x = torch.randn((size_in))
+    initial_scales = torch.randn((size_out, size_in))
+    initial_centers = torch.randn((size_out, size_in))
+
+    model = FuzzyLayer(initial_centers, initial_scales)
+    y_pred = model(x)
+    
+    assert len(y_pred) == size_out
+
+def test_1d_input_7d_output():
+    size_in = 1
+    size_out = 7
+    x = torch.randn((size_in))
+    initial_scales = torch.randn((size_out, size_in))
+    initial_centers = torch.randn((size_out, size_in))
+
+    model = FuzzyLayer(initial_centers, initial_scales)
+    y_pred = model(x)
+    
+    assert len(y_pred) == size_out
+
+def test_2d_input_1d_output():
+    size_in = 2
+    size_out = 1
+    x = torch.randn((size_in))
+    initial_scales = torch.randn((size_out, size_in))
+    initial_centers = torch.randn((size_out, size_in))
+
+    model = FuzzyLayer(initial_centers, initial_scales)
+    y_pred = model(x)
+    
+    assert len(y_pred) == size_out
+
+def test_2d_input_2d_output():
+    size_in = 2
+    size_out = 2
+    x = torch.randn((size_in))
+    initial_scales = torch.randn((size_out, size_in))
+    initial_centers = torch.randn((size_out, size_in))
+
+    model = FuzzyLayer(initial_centers, initial_scales)
+    y_pred = model(x)
+    
+    assert len(y_pred) == size_out
+
+def test_7d_input_7d_output():
+    size_in = 7
+    size_out = 7
+    x = torch.randn((size_in))
+    initial_scales = torch.randn((size_out, size_in))
+    initial_centers = torch.randn((size_out, size_in))
+
+    model = FuzzyLayer(initial_centers, initial_scales)
+    y_pred = model(x)
+    
+    assert len(y_pred) == size_out
+
---
+++ test_fuzzy_layer_inference.py
@@ -0,0 +1,33 @@
+from fuzzy_layer import FuzzyLayer
+import torch
+import pytest
+
+def test_case_1():
+    model = FuzzyLayer.fromcenters([[1,1], [10,10], [1,10], [10,1]])
+    x = torch.FloatTensor([1,1])
+    y = model(x)
+    assert y.detach().numpy() == pytest.approx([1,0,0,0], abs=0.1)
+
+def test_case_2():
+    model = FuzzyLayer.fromcenters([[1,1], [10,10], [1,10], [10,1]])
+    x = torch.FloatTensor([10,10])
+    y = model(x)
+    assert y.detach().numpy() == pytest.approx([0,1,0,0], abs=0.1)
+
+def test_case_3():
+    model = FuzzyLayer.fromcenters([[1,1], [10,10], [1,10], [10,1]])
+    x = torch.FloatTensor([1,10])
+    y = model(x)
+    assert y.detach().numpy() == pytest.approx([0,0,1,0], abs=0.1)
+
+def test_case_4():
+    model = FuzzyLayer.fromcenters([[1,1], [10,10], [1,10], [10,1]])
+    x = torch.FloatTensor([10,1])
+    y = model(x)
+    assert y.detach().numpy() == pytest.approx([0,0,0,1], abs=0.1)
+
+def test_case_5():
+    model = FuzzyLayer.fromcenters([[1,1], [2,2], [1,2], [2,1]])
+    x = torch.FloatTensor([1.5,1.5])
+    y = model(x)
+    assert y.detach().numpy() == pytest.approx([0.5,0.5,0.5,0.5], abs=1e-2)
