b2b9dac122168a58b6e352aed996acddc7b523c2
kenoma <gurov@yury.com>
2024-09-26 22:46:17 +0300

    almost separable features

--- test_fuzzy_bell_layer_inference.py
+++ test_fuzzy_bell_layer_inference.py
@@ -16,12 +16,19 @@ def test_case_2():
     assert y.shape == (1, 1)
     assert y.detach().numpy()[0] == pytest.approx([1], abs=0.1)
 
-def test_case_3():
+def test_case_3a():
     model = FuzzyBellLayer.from_centers([[0, 0]])
     x = torch.FloatTensor([[1, 1]])
     y = model(x)
     assert y.shape == (1, 1)
-    assert y.detach().numpy()[0] == pytest.approx([0.5], abs=0.1)
+    assert y.detach().numpy()[0] == pytest.approx([1.0/3.0], abs=0.1)
+    
+def test_case_3b():
+    model = FuzzyBellLayer.from_centers([[0]])
+    x = torch.FloatTensor([[1]])
+    y = model(x)
+    assert y.shape == (1, 1)
+    assert y.detach().numpy()[0] == pytest.approx([1.0/2.0], abs=0.1)
 
 def test_case_4():
     model = FuzzyBellLayer.from_centers([[0, 0]])
@@ -35,17 +42,17 @@ def test_case_5():
     x = torch.FloatTensor([[0,0],[0,0],[0,0]])
     y = model(x)
     assert y.shape == (3, 2)
-    assert y[0].detach().numpy() == pytest.approx([1, 0.41421354], abs=0.1)
-    assert y[1].detach().numpy() == pytest.approx([1, 0.41421354], abs=0.1)
-    assert y[2].detach().numpy() == pytest.approx([1, 0.41421354], abs=0.1)
+    assert y[0].detach().numpy() == pytest.approx([1, 1.0/3.0], abs=0.01)
+    assert y[1].detach().numpy() == pytest.approx([1, 1.0/3.0], abs=0.01)
+    assert y[2].detach().numpy() == pytest.approx([1, 1.0/3.0], abs=0.01)
 
 
 def test_case_6():
-    model = FuzzyBellLayer.from_centers([[0,0], [1,1]])
-    x = torch.FloatTensor([[0,0],[1, 1],[-1, -1]])
+    model = FuzzyBellLayer.from_centers([[0, 0], [1, 1]])
+    x = torch.FloatTensor([[0, 0], [1, 1], [-1, -1]])
     y = model(x)
     assert y.shape == (3, 2)
-    assert y[0].detach().numpy() == pytest.approx([1, 0.41421354], abs=0.1)
-    assert y[1].detach().numpy() == pytest.approx([0.41421354, 1], abs=0.1)
-    assert y[2].detach().numpy() == pytest.approx([ 0.41421354, 0.26120389], abs=0.1)
+    assert y[0].detach().numpy() == pytest.approx([ 1,        1.0/3.0], abs=0.1)
+    assert y[1].detach().numpy() == pytest.approx([ 1.0/3.0,  1], abs=0.1)
+    assert y[2].detach().numpy() == pytest.approx([ 1.0/3.0,  0.11111111], abs=0.01)
 
--- torchfuzzy/fuzzy_bell_layer.py
+++ torchfuzzy/fuzzy_bell_layer.py
@@ -8,7 +8,7 @@ class FuzzyBellLayer(torch.nn.Module):
 
     def __init__(self, initial_centers, initial_scales, initial_pow, trainable=True):
         """
-        mu_j(x,a,c) =1/( 1 + || (x - c) / a ||^(2*b) )
+        mu_j(x,a,c) =1/( 1 + || a.x  ||^(2*b) )
         """
         super().__init__()
 
@@ -17,10 +17,22 @@ class FuzzyBellLayer(torch.nn.Module):
 
         sizes = np.shape(initial_centers)
         self.size_out, self.size_in, *_ = sizes
-        self.a = nn.Parameter(initial_scales, requires_grad=trainable) 
-        self.b = nn.Parameter(initial_pow, requires_grad=trainable)
-        self.c = nn.Parameter(initial_centers, requires_grad=trainable)
 
+        const_row = np.zeros(self.size_in+1)
+        const_row[self.size_in] = 1
+        const_row = np.array([const_row]*self.size_out)
+        const_row = np.reshape(const_row, (self.size_out, 1, self.size_in+1))
+        self.c_r = nn.Parameter(torch.FloatTensor(const_row), requires_grad=False)
+        self.c_one = nn.Parameter(torch.FloatTensor([1]), requires_grad=False)
+        
+        self.scales = nn.Parameter(initial_scales, requires_grad=trainable)
+        self.b = nn.Parameter(initial_pow, requires_grad=trainable)
+        self.rots = []
+        for i in range(self.size_in - 1):
+            self.rots.append(nn.Parameter(torch.zeros((self.size_out, self.size_in - i - 1), requires_grad=trainable)))
+        self.rots = nn.ParameterList(self.rots)
+        self.centroids = nn.Parameter(initial_centers.reshape((self.size_out, self.size_in, 1)), requires_grad=trainable)    
+        
     @classmethod
     def from_dimensions(cls, size_in, size_out, trainable=True):
         initial_centers = torch.randn((size_out, size_in))
@@ -30,21 +42,21 @@ class FuzzyBellLayer(torch.nn.Module):
 
     @classmethod
     def from_centers(cls, initial_centers, trainable=True):
-        initial_centers =  torch.FloatTensor(initial_centers)
+        initial_centers = torch.FloatTensor(np.multiply(-1, initial_centers))
         initial_scales = torch.ones_like(initial_centers)
         initial_pow = torch.ones(initial_centers.shape[0])
         return cls(initial_centers, initial_scales, initial_pow, trainable)
     
     @classmethod
     def from_centers_and_scales(cls, initial_centers, initial_scales, trainable=True):
-        initial_centers =  torch.FloatTensor(initial_centers)
+        initial_centers = torch.FloatTensor(np.multiply(-1, initial_centers))
         initial_scales =  torch.FloatTensor(initial_scales)
         initial_pow = torch.ones(initial_centers.shape[0])
         return cls(initial_centers, initial_scales, initial_pow, trainable)
 
     @classmethod
     def from_centers_scales_and_pow(cls, initial_centers, initial_scales, initial_pow, trainable=True):
-        initial_centers =  torch.FloatTensor(initial_centers)
+        initial_centers = torch.FloatTensor(np.multiply(-1, initial_centers))
         initial_scales =  torch.FloatTensor(initial_scales)
         initial_pow = torch.FloatTensor(initial_pow)
         
@@ -53,16 +65,55 @@ class FuzzyBellLayer(torch.nn.Module):
     def forward(self, input: Tensor) -> Tensor:
         batch_size = input.shape[0]
         
-        rep_x = input.repeat_interleave(self.size_out, 0).reshape((batch_size, self.size_out, self.size_in))
-        rep_a = self.a.where(self.a > 1e-5, 1e-5)
-        rep_a = rep_a.repeat(batch_size, 1).reshape((batch_size, self.size_out, self.size_in))
-        rep_b = self.b.abs().repeat(batch_size, 1).reshape((batch_size, -1))
-        rep_c = self.c.repeat(batch_size, 1).reshape((batch_size, self.size_out, self.size_in))
+        A = self.get_scales_and_rot()
+        A = torch.cat((A, self.centroids), 2)
+        ta = torch.cat([A, self.c_r], 1)
+        repeated_one = self.c_one.repeat(batch_size, 1)
+        ext_x = torch.cat([input, repeated_one], 1)
         
-        fat = ((rep_x - rep_c) / rep_a).norm(2, dim =-1).pow(2 * rep_b)
+        tx = torch.transpose(ext_x, 0, 1)
+        mul = torch.matmul(ta, tx)
         
-        return 1/(1+fat)
+        rx = torch.norm(mul[:,:self.size_in], p=2, dim=1)
+        rx = rx.transpose(0,1).pow(2*self.b)
+        
+        memberships = 1/(1+rx)
+        return memberships
+    
+    def set_requires_grad_rot(self, requires_grad):
+        for i in range(self.size_in - 1):
+            self.rots[i].requires_grad = requires_grad
+
+    def set_requires_grad_scales(self, requires_grad):
+        self.scales.requires_grad = requires_grad
+
+    def set_requires_grad_centroids(self, requires_grad):
+        self.centroids.requires_grad = requires_grad
+    
+    def get_scales_and_rot(self):
+        scales = self.scales
+        A = torch.diag_embed(scales, 0)
+        for i in range(self.size_in - 1):
+            r = self.rots[i]
+            A = A + torch.diag_embed(r, i+1)
+            A = A + torch.diag_embed(r, i+1, -1, -2)
+        return A
     
     def get_centroids(self):
-        return self.c.detach().clone()
+        lh = self.get_scales_and_rot()
+        rh = self.centroids.squeeze(-1)
+        return torch.linalg.solve(lh, -rh)
+            
+    def get_transformation_matrix_eigenvals(self):
+        A = self.get_scales_and_rot()
+        return torch.linalg.eigvals(A)
+    
+    def get_transformation_matrix(self):
+        A = self.get_scales_and_rot()
+        A = torch.cat((A, self.centroids), 2)
+        ta = torch.cat([A, self.c_r], 1)
+        return ta
+    
+    
+
 
