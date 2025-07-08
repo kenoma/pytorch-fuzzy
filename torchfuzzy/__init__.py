2e3c95ddafbaa6ce8daeaefe6833c88ea952a539
gurov <gurov.yury@gmail.com>
2025-07-08 20:53:52 +0300

    no progress with ad

--- torchfuzzy/__init__.py
+++ torchfuzzy/__init__.py
@@ -1,5 +1,6 @@
 from .fuzzy_layer import FuzzyLayer
 from .fuzzy_bell_layer import FuzzyBellLayer
 from .defuzzy_linear import DefuzzyLinearLayer
+from .defuzzy_max import DefuzzyMaxLayer
 
-__all__ = ["FuzzyLayer", "FuzzyBellLayer", "DefuzzyLinearLayer"]
+__all__ = ["FuzzyLayer", "FuzzyBellLayer", "DefuzzyLinearLayer", "DefuzzyMaxLayer"]
